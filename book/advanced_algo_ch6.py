605eb4ceb876b1ae948574a8d4afceaf33a89d94
gitcoin_yoon <socozy7210@gmail.com>
2021-12-31 01:54:28 +0900

    quant book chapter 6 code

--- book/advanced_algo_ch6.py
+++ book/advanced_algo_ch6.py
@@ -1,10 +1,15 @@
+
+# QuantStart
+# Advanced Algorithmic Trading 
+# Michael L. Halls-Moore, PhD
+# Chapter 6
+
 import datetime
 import pprint
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import pandas_datareader as pdr
-import pymc3 as pd
+import pymc3 as pm
 import seaborn as sns
 
 def obtain_prices_df(ticker,start_date,end_date):
@@ -15,6 +20,32 @@ def obtain_prices_df(ticker,start_date,end_date):
     df["log_returns"] = np.log(df["returns"])
 
     return df
+
+def stochastic_volatility_model(df,samples):
+
+    model = pm.Model()
+    log_returns = np.array(df["log_returns"])
+
+    with model: 
+
+        sigma = pm.Exponential('sigma', 50.0, testval = 0.1)
+        nu = pm.Exponential('nu', 0.1)
+        s = pm.GaussianRandomWalk('s', sigma**-2, shape = len(log_returns))
+        logrets = pm.StudentT(
+
+            'logrets',
+            nu,
+            lam = pm.math.exp(-2.0*s),
+            observed = log_returns
+        )
+    
+    print("Fitting the stochastic volatility model...")
+    with model:
+
+        trace = pm.sample(samples)
+
+    pm.traceplot(trace, model.vars[:-1])
+    plt.show()
 
 def plot(df,ticker):
 
@@ -29,8 +60,9 @@ if __name__ == "__main__":
     start_date = datetime.datetime(2006,1,1)
     end_date = datetime.datetime(2015,12,31)
     ticker = "AMZN"
+    samples = 2000
 
     df = obtain_prices_df(ticker = ticker, start_date = start_date, end_date = end_date)
+    stochastic_volatility_model(df = df,samples = samples)
 
     print(df)
-    plot(df,ticker)
