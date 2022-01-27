fa182938f16a631083473dad8161db663aebb3c1
gitcoin_yoon <socozy7210@gmail.com>
2022-01-28 03:59:27 +0900

    add ohlcv chart command

--- scraping_pkg/data_manager.py
+++ scraping_pkg/data_manager.py
@@ -1,6 +1,7 @@
 
 from traceback import format_exception
 from h11 import Data
+from matplotlib.pyplot import close
 from pykrx import stock as krx
 from utils import Utils
 
@@ -21,9 +22,9 @@ class Data_Manager:
         return krx.get_market_ticker_list(date=date, market=market)
 
     @staticmethod
-    def get_market_cap_by_date(ticker=None, freq='d') -> list :
+    def get_transaction_amount(ticker=None, freq='d') -> list :
         
-        (_from, _to) = Utils.calculate_date()
+        (_from, _to) = Utils.get_date()
         df = krx.get_market_cap_by_date(
             _from, 
             _to, 
@@ -33,16 +34,52 @@ class Data_Manager:
 
         return df['거래대금'].values.tolist()
 
+    @staticmethod 
+    def is_upper_limit(ticker: str = None, freq: str ='d'):
+
+        (_from,_to) = Utils.get_date()
+        df = krx.get_market_ohlcv_by_date(
+                fromdate=_from,
+                todate=_to,
+                ticker=ticker,
+                freq=freq
+            )
+        start_price = df['시가'].tolist()
+        close_price = df['종가'].tolist()
+
+        return start_price,close_price
+
+    @staticmethod
+    def is_tail(ticker: str = None):
+        pass
+
     @staticmethod
     def get_ticker_name(ticker: str) -> str :
 
         return krx.get_market_ticker_name(ticker=ticker)
 
+
 if __name__ == "__main__":
     
     # print(Data_Manager().get_ticker())
-    print(Data_Manager.get_market_cap_by_date(
-        fromdate='20210127',
-        todate='20220127',
-        ticker='005930'
+    df = Data_Manager.is_upper_limit(ticker='005930')
+    start_price = df['시가'].tolist()
+    close_price = df['종가'].tolist()
+    print(df)
+    print('시가')
+    print(start_price)
+    print('종가')
+    print(close_price)
+    print(Utils.pct_change(
+        price_1=start_price[0],
+        price_2=close_price[1]
     ))
+    for index in range(len(df)-1):
+        if (
+            Utils.pct_change(
+                price_1=start_price[index],
+                price_2=close_price[index+1]
+            ) > 28.5
+        ):
+
+            print("true")
