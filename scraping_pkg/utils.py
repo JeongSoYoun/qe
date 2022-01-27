7ed0f2eb60a920346eead6005bf9ea4adea338e6
gitcoin_yoon <socozy7210@gmail.com>
2022-01-28 03:59:48 +0900

    add pct_change for price data

--- scraping_pkg/utils.py
+++ scraping_pkg/utils.py
@@ -5,7 +5,7 @@ from dateutil.relativedelta import relativedelta as before
 class Utils:
 
     @staticmethod
-    def calculate_date(period=None) -> list :
+    def get_date(period=None) -> list :
 
         """
         Return
@@ -15,6 +15,15 @@ class Utils:
         _from = _today - before(years=1)
         
         return [_from.strftime('%Y%m%d'), _today.strftime('%Y%m%d')]
+
+    @staticmethod
+    def pct_change(price_1, price_2) -> float:
+
+        if price_1 == 0:
+            return 0
+        
+        return (price_2 - price_1) / price_1 * 100
+        
 
 if __name__ == "__main__":
 
