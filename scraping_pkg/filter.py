4da05983dcd6dead887661812eb16033728c78ae
gitcoin_yoon <socozy7210@gmail.com>
2022-01-28 03:59:02 +0900

    fix some bugs

--- scraping_pkg/filter.py
+++ scraping_pkg/filter.py
@@ -1,5 +1,7 @@
 
 from unittest.util import sorted_list_difference
+
+from h11 import Data
 from data_manager import Data_Manager
 from scraping import Scraper
 from utils import Utils
@@ -7,22 +9,23 @@ import re
 
 FILTERD_INFO = ['시가총액', '유동비율']
 TRANSACTION_AMOUNT = 3e10
+PRICE_UPPER_LIMIT = 30
 
 def filter(data) -> list:
 
     """
-        시총 
-        - 3000억 이상 제외. 
+        Args
+        - data: DataFrame of scrpaed data.
 
-        3년 순이익률
-        - 3년 순이익률이 모두 마이너스면 제외.
+        Return
+        - filtered_ticker: List of filtered ticker.
 
-        거래 대금
-        - 300억 미만 제외.
-
-        상한가 여부
-        
-        긴꼬리 여부
+        Filtering Condition
+            - 시총: 3000억 이상 제외. 
+            - 3년 순이익률: 3년 순이익률이 모두 마이너스면 제외.
+            - 거래 대금: 300억 미만 제외.
+            - 상한가 여부: 1년중 상한가가 없으면 제외.
+            - 긴꼬리 양봉 여부: 1년중 긴꼬리 양봉 업으면 제외.
     """
 
     filtered_ticker = []
@@ -30,7 +33,6 @@ def filter(data) -> list:
 
         # market_cap
         market_cap = int(re.sub('[^\d]',"",data.loc[index]['시가총액']))
-
         if market_cap > 3000:
             continue
         
@@ -41,13 +43,29 @@ def filter(data) -> list:
             
         # 3년 순이익률
         profit_ratio_3years = data.loc[index]['3년 순이익률']
-        if float(sorted(profit_ratio_3years)[-1]) < 0.0:
-            continue
+        try:
+            profit_ratio = float(sorted(profit_ratio_3years)[-1])
+            if  profit_ratio < 0.0:
+                continue
+        except Exception as e:
+            print(f"You have error!. {e}")
         
         # 거래대금
         ticker = data.loc[index]['티커']
-        tx_amount = Data_Manager.get_market_cap_by_date(ticker=ticker)
+        tx_amount = Data_Manager.get_transaction_amount(ticker=ticker)
         if sorted(tx_amount)[-1] < TRANSACTION_AMOUNT:
+            continue
+
+        # 상한가 여부
+        (start_price, close_price) = Data_Manager.is_upper_limit(ticker=ticker)
+        max = len(start_price)-1
+        indicator = 0
+        for i in range(max):
+            if i == max:
+                indicator = i
+            if Utils.pct_change(start_price[i], close_price[i+1]) > PRICE_UPPER_LIMIT:
+                break
+        if indicator == len(start_price)-1:
             continue
 
         print(f"Ticker {ticker}:{Data_Manager.get_ticker_name(ticker)}")
